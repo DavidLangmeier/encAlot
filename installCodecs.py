6e6b6eb9ba80c5dfc2f470400be9009bf1396e4b
David Langmeier <dalangmeier@edu.aau.at>
2020-12-07 00:45:09 +0100

    fix clone and install routine for linux

--- installCodecs.py
+++ installCodecs.py
@@ -1,7 +1,6 @@
 # download and install codecs
 
-import wget
-import os, ssl, sys, subprocess
+import os, subprocess
 from pathlib import Path
 
 
@@ -10,31 +9,6 @@ gitHM = "https://vcgit.hhi.fraunhofer.de/jct-vc/HM.git"
 gitVTM = "https://vcgit.hhi.fraunhofer.de/jvet/VVCSoftware_VTM.git"
 gitVVenC = "https://github.com/fraunhoferhhi/vvenc.git"
 
-# paths for the codecs
-# pathHM = "encoders/HM"
-# pathVTM = "encoders/VVCSoftware_VTM"
-# pathVVenC = "encoders/vvenc"
-
-
-# bar_progress method which is invoked automatically from wget
-# def bar_progress(current, total, width=80):
-#    progress_message = "Downloading: %d%% [%d / %d] bytes" % (current / total * 100, current, total)
-#    sys.stdout.write("\r" + progress_message)
-#    sys.stdout.flush()
-
-
-# allow Python to download from websites with https protocol
-# if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
-#         getattr(ssl, '_create_unverified_context', None)):
-#     ssl._create_default_https_context = ssl._create_unverified_context
-
-# download .zip files to codec path
-# print("\n*** HEVC-HM download starting... ***")
-# wget.download(urlHM, pathHM, bar=bar_progress)
-# print("\n*** VVC-VTM download starting... ***")
-# wget.download(urlVTM, pathVTM, bar=bar_progress)
-# print("\n*** VVC-VVenC download starting... ***")
-# wget.download(urlVVenC, pathVVenC, bar=bar_progress)
 
 # clone the git repositories
 print("\n*** Cloning & installing codecs now ***")
@@ -42,21 +16,26 @@ os.chdir(Path("encoders"))
 
 print("\n*** cloning HEVC-HM git repository ***")
 subprocess.run(["git", "clone", gitHM])
-# os.chdir(Path("HM"))
-# subprocess.run(["make"])
-# os.chdir(Path(".."))
-# print("Current Working Directory ", os.getcwd())
+os.chdir(Path("HM"))
+subprocess.run(["mkdir", "build"])
+subprocess.run(["cd", "build"])
+subprocess.run(["cmake", "..", "-DCMAKE_BUILD_TYPE=Release"])
+subprocess.run(["make -j"])
+os.chdir(Path(".."))
 
 print("\n*** cloning VTM git repository ***")
 subprocess.run(["git", "clone", gitVTM])
-# os.chdir(Path("VVCSoftware_VTM"))
-# subprocess.run(["make"])
-# os.chdir(Path(".."))
-# print("Current Working Directory ", os.getcwd())
+os.chdir(Path("VVCSoftware_VTM"))
+subprocess.run(["mkdir", "build"])
+subprocess.run(["cd", "build"])
+subprocess.run(["cmake", "..", "-DCMAKE_BUILD_TYPE=Release"])
+subprocess.run(["make -j"])
+os.chdir(Path(".."))
 
 print("\n*** cloning VVenC git repository ***")
 subprocess.run(["git", "clone", gitVVenC])
-# os.chdir(Path("vvenc"))
-# subprocess.run(["make"])
-# os.chdir(Path(".."))
-# print("Current Working Directory ", os.getcwd())
+os.chdir(Path("vvenc"))
+subprocess.run(["make"])
+os.chdir(Path(".."))
+
+print("\n*** all codecs installed and ready ***")
